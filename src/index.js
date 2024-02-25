c6a1bc8c0cc1d1ca0d58b357cd3e3fd940630a1a
omonillokh yuldashev <135029556+moonlifee@users.noreply.github.com>
2024-02-25 21:44:13 +0500

    updated footer andr router dom\/added redux tool kit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// import { Provider } from 'react-redux';
+// import { store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
-import { BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  // <Provider store={store}>
+  <Provider store={store}>
+    <BrowserRouter>
+
 
       <App />
 
-  </BrowserRouter>
+    </BrowserRouter>
+  </Provider>
 );
